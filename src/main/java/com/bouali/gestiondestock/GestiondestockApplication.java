5c8fa8aac571ae2eb32c05ed6f8f7b106e89149b
mehdi <mehdi.dellegi@esprit.tn>
2022-11-23 09:18:49 -0800

    test push with intelligi

--- src/main/java/com/bouali/gestiondestock/GestiondestockApplication.java
+++ src/main/java/com/bouali/gestiondestock/GestiondestockApplication.java
@@ -11,7 +11,7 @@ public class GestiondestockApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestiondestockApplication.class, args);
 		System.out.println("GestiondestockApplication ==> main" );
-		System.out.println("Ended Up with entity Section 4 completed!" );
+		System.out.println("test push with intelligi" );
 	}
 
 }
