10476eec90fed8e88b43aeb3f47ee1a843a6eb83
unknown <mehdi.dellegi@esprit.tn>
2022-11-23 09:01:46 -0800

    Ended Up with entity Section 4 completed

--- src/main/java/com/bouali/gestiondestock/GestiondestockApplication.java
+++ src/main/java/com/bouali/gestiondestock/GestiondestockApplication.java
@@ -11,6 +11,7 @@ public class GestiondestockApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestiondestockApplication.class, args);
 		System.out.println("GestiondestockApplication ==> main" );
+		System.out.println("Ended Up with entity Section 4 completed!" );
 	}
 
 }
